09620bfffed41fbc403bbe6de9a4ee12c84ef310
Ivann <212ivann.rambaud@gmail.com>
2026-03-06 12:32:34 +0100

    change link nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,8 +7,8 @@ function loadPartial(id, file, callback) {
         });
 }
 
-loadPartial("nav", "./partials/nav.html", setActiveNav);
-loadPartial("footer", "./partials/footer.html");
+loadPartial("nav", "/NexoraWeb/partials/nav.html", setActiveNav);
+loadPartial("footer", "/NexoraWeb/partials/footer.html");
 
 var projects = [
           {
